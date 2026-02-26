dfe210901c92a47bda5def37f4e1b08e4601929a
Nargiza Sulaimankulova <nargizasulaimankulova@Mac.lan>
2026-02-26 12:00:39 -0600

    second file

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+:ublic class HelloWorld {
+She is the besjhgdsljhgl
+
+    public static void main(String[] args){
+         System.out.println("Hello World!");
+         System.out.println("Hello Codewise!");
+    }
+}
